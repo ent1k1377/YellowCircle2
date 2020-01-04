294de6a51254024d0d8d3fbf1ab0917669c80159
ent1k <ent1k1337@yandex.ru>
2020-01-05 00:45:54 +0300

    Yellow Circle

--- git_file1.py
+++ git_file1.py
@@ -1,23 +1,24 @@
 import sys
 import random
-from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
 from PyQt5.QtGui import QPainter, QColor, QPainterPath
+from ui_file import Ui_MainWindow
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('yellowcircle.ui', self)
+        self.setupUi(self)
         self.setWindowTitle('Random yellow circle')
-        self.pushButton_1.clicked.connect(self.Button)
+        self.pushButton.clicked.connect(self.Button)
         self.x = False
         self.lst1 = []
 
     def Button(self):
         self.x = True
         self.lst1.append([random.randint(1, 500), random.randint(30, 500), random.randint(1, 500),
-                       random.randint(30, 500)])
+                          random.randint(30, 500), random.randint(1, 255), random.randint(1, 255),
+                          random.randint(1, 255)])
 
     def paintEvent(self, event):
         qp = QPainter()
@@ -28,7 +29,7 @@ class MyWidget(QMainWindow):
     def rightClick(self, qp):
         if self.x:
             for i in range(len(self.lst1)):
-                qp.setBrush(QColor('yellow'))
+                qp.setBrush(QColor(self.lst1[i][4], self.lst1[i][5], self.lst1[i][6]))
                 qp.drawEllipse(self.lst1[i][0], self.lst1[i][1], self.lst1[i][2], self.lst1[i][3])
             self.update()
             self.Button()
