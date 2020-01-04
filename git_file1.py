a9fbdeb3b24cf44c84b27c0d5c83251ebb4a56c8
ent1k <ent1k1337@yandex.ru>
2020-01-04 21:15:50 +0300

    Yellow Circle

--- git_file1.py
+++ git_file1.py
@@ -1,7 +1,40 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
+import sys
+import random
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+from PyQt5.QtGui import QPainter, QColor, QPainterPath
 
 
-if __name__ == '__main__':
-    main()
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('yellowcircle.ui', self)
+        self.setWindowTitle('Random yellow circle')
+        self.pushButton_1.clicked.connect(self.Button)
+        self.x = False
+        self.lst1 = []
+
+    def Button(self):
+        self.x = True
+        self.lst1.append([random.randint(1, 500), random.randint(30, 500), random.randint(1, 500),
+                       random.randint(30, 500)])
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.rightClick(qp)
+        qp.end()
+
+    def rightClick(self, qp):
+        if self.x:
+            for i in range(len(self.lst1)):
+                qp.setBrush(QColor('yellow'))
+                qp.drawEllipse(self.lst1[i][0], self.lst1[i][1], self.lst1[i][2], self.lst1[i][3])
+            self.update()
+            self.Button()
+
+
+app = QApplication(sys.argv)
+ex = MyWidget()
+ex.show()
+sys.exit(app.exec_())
